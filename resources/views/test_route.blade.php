70c9a1d8d2748162cd1b2825612f4a6140ac92b9
Guiliard <camposrafa806@gmail.com>
2024-11-18 20:47:09 -0300

    Testing routes

---
+++ resources/views/test_route.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple PHP Test</title>
+</head>
+<body>
+    <h1>Welcome to the Route Test</h1>
+
+    <?php
+        echo "<p>This content was generated using PHP!</p>";
+        echo "<p>Current date: " . date('Y-m-d') . "</p>";
+    ?>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test_route', function () {
+    return view('test_route');
+});
+
+Route::get('/test_product/{id}', function ($id) {
+    return "The product id is: " . $id;
+});
