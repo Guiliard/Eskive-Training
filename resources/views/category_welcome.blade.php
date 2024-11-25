b8b1530ad28409477ad9ef1e92ca2d2cf049bed8
Guiliard <camposrafa806@gmail.com>
2024-11-25 17:01:37 -0300

    Add new routes of welcome page

--- resources/views/category_welcome.blade.php
+++ resources/views/category_welcome.blade.php
@@ -9,6 +9,12 @@
     <h1>Bem-vindo ao Gerenciador de Categorias</h1>
     <a href="{{ route('category.show_all') }}">Ver Todas as Categorias</a>
     <br><br>
+    <a href="{{ route('category.show_one') }}">Ver Uma Categoria Específica</a>
+    <br><br>
     <a href="{{ route('category.store') }}">Cadastrar Nova Categoria</a>
+    <br><br>
+    <a href="{{ route('category.update') }}">Atualizar Categoria Específica</a>
+    <br><br>
+    <a href="{{ route('category.delete') }}">Deletar Categoria Específica</a>
 </body>
 </html>
--- resources/views/product_welcome.blade.php
+++ resources/views/product_welcome.blade.php
@@ -9,7 +9,14 @@
     <h1>Bem-vindo ao Gerenciador de Produtos</h1>
     <a href="{{ route('product.show_all') }}">Ver Todos os Produtos</a>
     <br><br>
+    <a href="{{ route('product.show_one') }}">Ver Um Produto Específico</a>
+    <br><br>
     <a href="{{ route('product.store') }}">Cadastrar Novo Produto</a>
+    <br><br>
+    <a href="{{ route('product.update') }}">Atualizar Produto Específico</a>
+    <br><br>
+    <a href="{{ route('product.delete') }}">Deletar Produto Específico</a>
+
 </body>
 </html>
 
