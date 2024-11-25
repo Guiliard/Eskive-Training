76501daa1a813705a5346bbc221030d42f99b2af
Guiliard <camposrafa806@gmail.com>
2024-11-25 16:43:22 -0300

    Remove tests of last week

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test_route', function () {
-    return view('test_route');
-});
-
-Route::get('/test_product/{id?}', function ($id = 0) {
-    return "The product id is: " . $id;
-});
-
-Route::group(['prefix' => 'test_controller'], function () {
-    Route::get('/', [TestController::class, 'index'])->name('test_controller.index');
-
-    Route::get('/product/{id?}', [TestController::class, 'show'])->name('test_controller.show');
-
-    Route::get('/resource', [ResourceController::class, 'index'])->name('test_controller.resource');
-});
-
 Route::group(['prefix' => 'product'], function () {
     Route::get('/', [ProductController::class, 'welcome'])->name('product.welcome');
 
